25e6ce6bf896e64976fc30b696c5130e37e8a94a
MalpikCruE28 <86462951+MalpikCruE28@users.noreply.github.com>
2023-04-25 21:38:29 -0600

    BD en firebase
    BD en fire base

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: "AIzaSyCxYLyP8-brh9sePcLVv8porCUA-eFntEk",
+  authDomain: "mi-app-6bda0.firebaseapp.com",
+  projectId: "mi-app-6bda0",
+  storageBucket: "mi-app-6bda0.appspot.com",
+  messagingSenderId: "445035317494",
+  appId: "1:445035317494:web:28de0c041faaf75c88c4d7",
+  measurementId: "G-6P0JPX6GS1"
 };
 
 /*
